28a10731e4dac6df1b57e7183062bd6a730f3494
Emmanuel WALDNER <emmanuel.waldner@gmail.com>
2021-08-30 14:56:46 +0200

    Correction Developpement Branch et amélioration

--- oc_lettings_site/settings/base.py
+++ oc_lettings_site/settings/base.py
@@ -116,6 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
 
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
