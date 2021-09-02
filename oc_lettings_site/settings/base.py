4e94ae97cdf38052da3e2bc6db1bde35cc0312de
Emmanuel WALDNER <emmanuel.waldner@gmail.com>
2021-09-02 22:28:07 +0200

    Nettoyage requirements.txt et correction static partie admin via URL

--- oc_lettings_site/settings/base.py
+++ oc_lettings_site/settings/base.py
@@ -116,14 +116,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
 
-# Static files (CSS, JavaScript, Images)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
